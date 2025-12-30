dfa732adeb01ef16c3cd1c448e98fbfdc872c631
guranshchugh-9 <guranshchug07@gmail.com>
2025-12-30 14:24:30 +0530

    learnt apps

---
+++ Djangofolder/gura/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GuraConfig(AppConfig):
+    name = 'gura'
---
+++ Djangofolder/gura/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.app1,name='app1'),
+    # path('/order',views.app1,name='order')
+]
